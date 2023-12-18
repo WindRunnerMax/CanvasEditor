a39ba361a312fb455c88bd8e0c79e64096ceb79a
WindrunnerMax <651525974@qq.com>
2023-12-18 23:29:33 +0800

    feat: fine-tuning structure

--- packages/plugins/example/index.tsx
+++ packages/plugins/example/index.tsx
@@ -1,3 +1,7 @@
 import ReactDOM from "react-dom";
+import { PKG as PKGCore } from "sketching-core";
+import { PKG as PKGDelta } from "sketching-delta";
+import { PKG as PKGExample } from "sketching-utils";
 
-ReactDOM.render(<div />, document.getElementById("root"));
+console.log(PKGCore, PKGDelta, PKGExample);
+ReactDOM.render(<div>Example</div>, document.getElementById("root"));
---
+++ packages/plugins/rspack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const { default: HtmlPlugin } = require("@rspack/plugin-html");
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isDev = process.env.NODE_ENV === "development";
+
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  context: __dirname,
+  entry: {
+    index: "./example/index.tsx",
+  },
+  plugins: [
+    new CopyPlugin([{ from: "./example/public", to: "." }]),
+    new HtmlPlugin({
+      filename: "index.html",
+      template: "./example/public/index.html",
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+    },
+  },
+  builtins: {
+    define: {
+      "__DEV__": JSON.stringify(isDev),
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
+    },
+    pluginImport: [
+      {
+        libraryName: "@arco-design/web-react",
+        customName: "@arco-design/web-react/es/{{ member }}",
+        style: true,
+      },
+    ],
+  },
+  module: {
+    rules: [
+      { test: /\.svg$/, type: "asset" },
+      {
+        test: /\.(m|module).scss$/,
+        use: [{ loader: "sass-loader" }],
+        type: "css/module",
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                javascriptEnabled: true,
+                importLoaders: true,
+                localIdentName: "[name]__[hash:base64:5]",
+              },
+            },
+          },
+        ],
+        type: "css",
+      },
+    ],
+  },
+  target: "es5",
+  devtool: isDev ? "source-map" : false,
+  output: {
+    publicPath: "/",
+    chunkLoading: "jsonp",
+    chunkFormat: "array-push",
+    filename: isDev ? "[name].js" : "[name].[hash].js",
+    path: path.resolve(__dirname, "build/static"),
+  },
+};
+
+// https://www.rspack.dev/
