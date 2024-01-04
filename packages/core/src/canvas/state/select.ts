a53f17d7c4dde71165087e7b6c359299f3b359ab
WindrunnerMax <651525974@qq.com>
2024-01-04 21:08:41 +0800

    chore: select convergence area judgment

--- packages/core/src/canvas/state/select.ts
+++ packages/core/src/canvas/state/select.ts
@@ -17,7 +17,6 @@ import {
   THE_CONFIG,
   THE_DELAY,
 } from "../utils/constant";
-import { isInSelectRange } from "../utils/is";
 import { BLUE } from "../utils/palette";
 import { drawRect } from "../utils/shape";
 
@@ -63,7 +62,7 @@ export class SelectNode extends Node {
     // 这里需要用原生事件绑定 需要在选区完成后再执行 否则交互上就必须要先点选再拖拽
     this.editor.event.off(EDITOR_EVENT.MOUSE_MOVE, this.onMouseMoveController);
     // 选区 & 严格点击区域判定
-    if (!this.editor.selection.get() || !isInSelectRange(Point.from(e), this.range)) {
+    if (!this.editor.selection.get() || !this.isInSelectRange(Point.from(e), this.range)) {
       return void 0;
     }
     this.landing = Point.from(e);
@@ -129,5 +128,45 @@ export class SelectNode extends Node {
       drawRect(ctx, { x, y, width, height, borderColor: BLUE });
       this.children.forEach(node => node.drawingMask?.(ctx));
     }
+  };
+
+  private isInSelectRange = (point: Point, range: Range) => {
+    // 严格判断点击区域 选区的`Range`需要排除八向占位
+    const { startX, startY, endX, endY } = range.flat();
+    const center = range.center();
+    const { x, y } = point;
+    const offset = RESIZE_OFS / 2;
+    const startX1OFS = startX + RESIZE_OFS;
+    const startY1OFS = startY + RESIZE_OFS;
+    const endX0OFS = endX - RESIZE_OFS;
+    const endY0OFS = endY - RESIZE_OFS;
+    const startX1Offset = startX + offset;
+    const endX0Offset = endX - offset;
+    const startY1Offset = startY + offset;
+    const endY0Offset = endY - offset;
+    const centerY0OFS = center.y - RESIZE_OFS;
+    const centerY1OFS = center.y + RESIZE_OFS;
+    const centerX0OFS = center.x - RESIZE_OFS;
+    const centerX1OFS = center.x + RESIZE_OFS;
+    if (
+      // `Range`范围内
+      x >= startX &&
+      x <= endX &&
+      y >= startY &&
+      y <= endY &&
+      // 排除对角线向占位
+      !(x <= startX1OFS && y <= startY1OFS) &&
+      !(x >= endX0OFS && y <= startY1OFS) &&
+      !(x <= startX1OFS && y >= endY0OFS) &&
+      !(x >= endX0OFS && y >= endY0OFS) &&
+      // 排除中心线向占位
+      !(x <= startX1Offset && y >= centerY0OFS && y <= centerY1OFS) &&
+      !(x >= endX0Offset && y >= centerY0OFS && y <= centerY1OFS) &&
+      !(y <= startY1Offset && x >= centerX0OFS && x <= centerX1OFS) &&
+      !(y >= endY0Offset && x >= centerX0OFS && x <= centerX1OFS)
+    ) {
+      return true;
+    }
+    return false;
   };
 }
