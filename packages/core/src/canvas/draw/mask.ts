f8b42b54b33e9d63fa3c024386df15f6a13dc0f0
WindrunnerMax <651525974@qq.com>
2024-01-07 11:07:23 +0800

    fix: batching drawing flicker

--- packages/core/src/canvas/draw/mask.ts
+++ packages/core/src/canvas/draw/mask.ts
@@ -10,11 +10,17 @@ import { CURSOR_STATE } from "../utils/constant";
 export class Mask {
   private canvas: HTMLCanvasElement;
   public ctx: CanvasRenderingContext2D;
+  private timer: NodeJS.Timeout | null;
+  private range: Range | null;
+  private effects: Set<Node> | null;
 
   constructor(private editor: Editor, private engine: Canvas) {
     // `Mask`绘制的是`Node`
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+    this.timer = null;
+    this.range = null;
+    this.effects = null;
   }
 
   public onMount(dom: HTMLDivElement, ratio: number) {
@@ -46,21 +52,37 @@ export class Mask {
     return effects;
   }
 
+  private batchDrawing(effects: Set<Node>, range: Range) {
+    // COMPAT: 防止瞬时多次绘制时闪动
+    this.range = this.range ? this.range.compose(range) : range;
+    this.effects = new Set([...(this.effects || []), ...effects]);
+    if (!this.timer) {
+      this.timer = setTimeout(() => {
+        const currentRange = this.range || range;
+        const currentEffects = this.effects || effects;
+        const { x, y, width, height } = currentRange.rect();
+        // 只绘制受影响的节点并且裁剪多余位置
+        this.clear(currentRange);
+        this.ctx.save();
+        this.ctx.beginPath();
+        this.ctx.rect(x, y, width, height);
+        this.ctx.clip();
+        currentEffects.forEach(node => node.drawingMask?.(this.ctx));
+        this.ctx.closePath();
+        this.ctx.restore();
+        this.timer = null;
+        this.range = null;
+        this.effects = null;
+      }, 16);
+    }
+  }
+
   public drawingEffect(range: Range) {
     // COMPAT: 选区范围未能完全覆盖
     const current = range.zoom(this.editor.canvas.devicePixelRatio);
     // 增量绘制`range`范围内的节点
     const effects = this.collectEffects(current);
-    const { x, y, width, height } = current.rect();
-    // 只绘制受影响的节点并且裁剪多余位置
-    this.clear(current);
-    this.ctx.save();
-    this.ctx.beginPath();
-    this.ctx.rect(x, y, width, height);
-    this.ctx.clip();
-    effects.forEach(node => node.drawingMask?.(this.ctx));
-    this.ctx.closePath();
-    this.ctx.restore();
+    this.batchDrawing(effects, current);
   }
 
   // ====== Cursor State ======
--- packages/core/src/event/bus/index.ts
+++ packages/core/src/event/bus/index.ts
@@ -38,7 +38,7 @@ export class EventBus {
   public off<T extends EventMapKeys>(key: T, listener: Listener<T>) {
     const handler = this.listeners[key];
     if (!handler) return void 0;
-    // COMPAT: 不能直接`splice` 可能会导致事件不能正常触发
+    // COMPAT: 不能直接`splice` 可能会导致`trigger`打断`forEach`
     const next = handler.filter(item => item.listener !== listener);
     (this.listeners[key] as Handler<T>[]) = next;
   }
